0bb760dc2cd53c4f665554a0e84821b507556599
xiaofei <alibaba22001@163.com>
2023-12-01 14:56:46 +0800

    update

---
+++ compile_go.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#================================================================
+#   
+#   
+#   文件名称：compile_go.sh
+#   创 建 者：肖飞
+#   创建日期：2023年11月05日 星期日 12时21分14秒
+#   修改日期：2023年11月09日 星期四 20时36分57秒
+#   描    述：
+#
+#================================================================
+set -x
+function main() {
+	export GOOS=linux
+	export GOARCH=amd64
+	proxychains bash libs/get_source.sh
+	bash libs/build_go.sh
+	#bash libs/build_public_res.sh
+}
+
+main $@
