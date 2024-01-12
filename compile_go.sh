add4bda518fb21f9590697aff5ab2616b406411c
xiaofei <alibaba22001@163.com>
2024-01-12 15:46:00 +0800

    update

--- compile_go.sh
+++ compile_go.sh
@@ -6,7 +6,7 @@
 #   文件名称：compile_go.sh
 #   创 建 者：肖飞
 #   创建日期：2023年11月05日 星期日 12时21分14秒
-#   修改日期：2023年11月09日 星期四 20时36分57秒
+#   修改日期：2024年01月12日 星期五 15时45分55秒
 #   描    述：
 #
 #================================================================
@@ -14,7 +14,8 @@ set -x
 function main() {
 	export GOOS=linux
 	export GOARCH=amd64
-	proxychains bash libs/get_source.sh
+	# proxychains bash libs/get_source.sh
+	bash libs/get_source.sh
 	bash libs/build_go.sh
 	#bash libs/build_public_res.sh
 }
